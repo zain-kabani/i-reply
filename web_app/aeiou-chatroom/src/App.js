c46928ebeb2481b0e9fdeb952a83a7522ef27bbd
XJ <15840839+PigwidgeonDeluxe@users.noreply.github.com>
2020-05-17 02:42:41 -0400

    front-end: created landing page, login page. TODO: fix login page routing, and form verification

--- web_app/aeiou-chatroom/src/App.js
+++ web_app/aeiou-chatroom/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 import './App.css';
+import LoginPage from "./LoginPage.js"
 import ChatBox from "./ChatBox.js"
 
 function App() {
+
   return (
+    /*
     <div className="App">
       <ChatBox />
+    </div>
+    */
+
+    <div className="App">
+      <Router>
+        <Switch>
+
+          <Route path="/LoginPage">
+            <LoginPage />
+          </Route>
+
+          <Route path="/">
+            <Link  to="/LoginPage">Login</Link>
+          </Route>
+
+          <Route path="/ChatBox"> 
+              <ChatBox />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
