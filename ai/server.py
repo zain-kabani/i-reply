132519834a885a140c7fe603faf4be514fd3c57a
zain-kabani <zain.kabani97@gmail.com>
2020-05-17 02:38:18 -0400

    changes to server.py

--- ai/server.py
+++ ai/server.py
@@ -1,38 +1,105 @@
 from flask import Flask, flash, request, redirect, url_for, jsonify, Response
 from werkzeug.utils import secure_filename
 import os
-# import JSON
+from google.cloud import storage
+from oauth2client.service_account import ServiceAccountCredentials
+import zipfile
+from data_clean import cleanup
+import time
 
-UPLOAD_FOLDER = ''
-ALLOWED_EXTENSIONS = {'zip'}
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
+client = storage.Client()
+bucket = client.get_bucket('ryerson-hacks-2020-aiplatform')
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+old_time = time.time()
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
+    print("HERE")
+    name = request.form.get("name")
+    cleanup("data/messages")
+    
+    
+    blob = bucket.blob('582C63D4AD74EA354E4F84EC2B4BADDB.txt')
+    blob.upload_from_filename('fbMessages.txt')
 
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return Response("{'err':'File not found'}", status=400, mimetype='application/json')
-        
-        file = request.files['file']
+    return Response("{'message': 'file uploaded successfully!'}", status=200, mimetype='application/json')
 
-        if file and allowed_file(file.filename):
-            print("HERE")
-            filename = secure_filename(file.filename)
-            print(filename)
-            print(request.form.get("name"))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            return Response("{'message': 'file uploaded successfully!'}", status=200, mimetype='application/json')
+@app.route('/check_done', methods=['GET'])
+def check_done():
+
+    if time.time() - old_time > 5:
+        return Response("{'done': True}", status=200, mimetype='application/json')
     else:
-        return jsonify(
-            username="HI"
-        )
+        return Response("{'done': False}", status=200, mimetype='application/json')
+
+    # check if the post request has the file part
+    # if 'file' not in request.files:
+    #     flash('No file part')
+    #     return Response("{'err':'File not found'}", status=400, mimetype='application/json')
+    
+    # with open("zip_data/thing.zip", "bw") as f:
+    #     chunk_size = 4096
+    #     while True:
+    #         chunk = request.stream.read(chunk_size)
+    #         if len(chunk) == 0:
+    #             break
+    #         f.write(chunk)
+    
+    # with zipfile.ZipFile("zip_data/thing.zip", 'r') as zip_ref:
+    #     zip_ref.extractall("zip_data")
+    
+
+    # print("HERE3")
+    # file = request.files['file']
+    # print("BRUH")
+    # if file and allowed_file(file.filename):
+    #     print("HERE2")
+    #     filename = secure_filename(file.filename)
+
+    #     print(filename)
+    #     print(name)
+    #     file.save(os.path.join("zip_data", filename))
+    #     # clean_file(app.config['UPLOAD_FOLDER'], filename))
+
+    
+
+# def upload_file_to_gcp(path):
+
+# credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json')
+# client = storage.Client()
+# bucket = client.get_bucket('ryerson-hacks-2020-aiplatform')
+# blob = bucket.blob(file_name)
+# blob.upload_from_filename(file_name)
+
+
+    # check if the post request has the file part
+    # if 'file' not in request.files:
+    #     flash('No file part')
+    #     return Response("{'err':'File not found'}", status=400, mimetype='application/json')
+    
+    # with open("zip_data/thing.zip", "bw") as f:
+    #     chunk_size = 4096
+    #     while True:
+    #         chunk = request.stream.read(chunk_size)
+    #         if len(chunk) == 0:
+    #             break
+    #         f.write(chunk)
+    
+    # with zipfile.ZipFile("zip_data/thing.zip", 'r') as zip_ref:
+    #     zip_ref.extractall("zip_data")
+    
+
+    # print("HERE3")
+    # file = request.files['file']
+    # print("BRUH")
+    # if file and allowed_file(file.filename):
+    #     print("HERE2")
+    #     filename = secure_filename(file.filename)
+
+    #     print(filename)
+    #     print(name)
+    #     file.save(os.path.join("zip_data", filename))
+    #     # clean_file(app.config['UPLOAD_FOLDER'], filename))
